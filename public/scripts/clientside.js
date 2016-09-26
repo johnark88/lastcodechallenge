b946a9a944218fa3fc0a55502f1b33c2f2176469
John Arkema <john.arkema@gmail.com>
2016-09-26 15:45:04 -0500

    angular controllers

--- public/scripts/clientside.js
+++ public/scripts/clientside.js
@@ -1,3 +1,19 @@
 console.log('sourced');
 
 var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider){
+  console.log('NG ');
+    $routeProvider.
+      when("/home", {
+        templateUrl: "/partials/home.html",
+        controller: "homeController"
+      }).
+      when("/add", {
+        templateUrl: "/partials/addNewHero.html",
+        controller: "addNewHeroController"
+      }).
+      otherwise({
+        redirectTo: "/home"
+      });
+}]);
---
+++ public/scripts/controllers/addNewHeroController.js
@@ -0,0 +1,8 @@
+myApp.controller('addNewHeroController',['$scope','$http',function($scope,$http){
+  console.log('addNewHero controller');
+
+  $scope.addHero = function (){
+
+
+  }
+}]);
