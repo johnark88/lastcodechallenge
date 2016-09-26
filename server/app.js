0b9af45e81af472dbc033ffc27bd68a72749405f
John Arkema <john.arkema@gmail.com>
2016-09-26 14:30:29 -0500

    index server route, index is served ok

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var urlencodedParser = bodyParser.urlencoded( { extended: true } );
 
+
+
+var index = require('./routes/index');
+app.use('/', index);
+
 var portDecision = process.env.PORT || 4242;
 
 app.use(bodyParser.json());
@@ -12,3 +17,6 @@ app.use(bodyParser.json());
 app.listen(portDecision, function(){
   console.log('Listening on: ', portDecision);
 });
+
+//use static file
+app.use(express.static('public'));
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var router = require('express').Router();
+var path = require('path');
+var express =require('express');
+
+//app.get --  serves the index -- get params
+router.get("/", function(req,res){
+    console.log("Here is the property: ", req.params);
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+
+});
+
+module.exports = router;
